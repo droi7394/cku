002de16e43ea6b56817321b5f1ec41a6063eeaba
DESKTOP-8E228OB\zhl22 <droi.7394@gmail.com>
2021-04-11 23:57:31 +0900

    주석추가

--- rc_car/1_Motor_driver/1_Motor_driver.ino
+++ rc_car/1_Motor_driver/1_Motor_driver.ino
@@ -9,7 +9,7 @@ void SmartCar_Go();
 void SmartCar_Back();
 
 
-int E_carSpeed = 153; // 최대 속도의  60 %
+int E_carSpeed = 153; // 최대 속도의  60 %, 범위 (0~255)
 
 void setup() {
   pinMode(RightMotor_E_pin, OUTPUT);        // 출력모드로 설정
@@ -30,24 +30,24 @@ void loop() {
 
 void SmartCar_Go() {       // 전진
   Serial.println("Forward");
-  digitalWrite(RightMotor_1_pin, HIGH);
-  digitalWrite(RightMotor_2_pin, LOW);
-  digitalWrite(LeftMotor_3_pin, HIGH);
-  digitalWrite(LeftMotor_4_pin, LOW);
+  digitalWrite(RightMotor_1_pin, HIGH);  // 우측앞바퀴 - 정회전
+  digitalWrite(RightMotor_2_pin, LOW);   // 우측 뒷바퀴 - 정회전
+  digitalWrite(LeftMotor_3_pin, HIGH);   // 좌측 앞바퀴 -정회전
+  digitalWrite(LeftMotor_4_pin, LOW);    // 좌측뒷바퀴  - 정회전
 
-  for (int i = 0; i <= E_carSpeed; i = i + 5) {
-    analogWrite(RightMotor_E_pin, i);     
-    analogWrite(LeftMotor_E_pin, i);
+  for (int i = 0; i <= E_carSpeed; i = i + 5) {  // 제한된 최고속도까지 5씩 속도를 증가시킨다.
+    analogWrite(RightMotor_E_pin, i);            // 우측바퀴 속도 제어
+    analogWrite(LeftMotor_E_pin, i);             // 좌측바퀴 속도 제어
     delay(20);
   }
 }
 
 void SmartCar_Back() {       // 후진
   Serial.println("Backward");
-  digitalWrite(RightMotor_1_pin, LOW);
-  digitalWrite(RightMotor_2_pin, HIGH);
-  digitalWrite(LeftMotor_3_pin, LOW);
-  digitalWrite(LeftMotor_4_pin, HIGH);
+  digitalWrite(RightMotor_1_pin, LOW);   // 역회전
+  digitalWrite(RightMotor_2_pin, HIGH);  // 역회전
+  digitalWrite(LeftMotor_3_pin, LOW);    // 역회전
+  digitalWrite(LeftMotor_4_pin, HIGH);   // 역회전
 
   for (int i = 0; i <= E_carSpeed; i = i + 5) {
     analogWrite(RightMotor_E_pin, i);     
