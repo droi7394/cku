f932dcf9b2ffe09adf42dec1c00c625d9d71129c
droi7394 <81270234+droi7394@users.noreply.github.com>
2021-04-12 00:15:10 +0900

    Update 3_Servo_motor.ino
    서보모터가 돌아갈 때 180 -> 90 방향으로 가도록 수정함.

--- rc_car/3_Servo_motor/3_Servo_motor.ino
+++ rc_car/3_Servo_motor/3_Servo_motor.ino
@@ -7,10 +7,12 @@ void setup() {
 }
 
 void loop() {
-  myservo.write(0);
-  delay(3000);
-  myservo.write(90);
-  delay(3000);
-  myservo.write(180);
+  myservo.write(0);   // 서보모터 각도 0 도
+  delay(3000);        // 3초 대기
+  myservo.write(90);  // 서보모터 각도 90도
+  delay(3000);        // 3초 대기
+  myservo.write(180); // 서보모터 각도 180 도
+  delay(3000);        // 3초 대기
+  myservo.write(90);  // 서보모터 각도 90도
   delay(3000);
 } 
